6611b95c774eb215d83a186e20e5881cde17e5ca
Adin Hodovic <hodovicadin@gmail.com>
2018-09-21 22:21:08 +0200

    Udpate

--- sync-ambrosus-ops.sh
+++ sync-ambrosus-ops.sh
@@ -1,7 +1,15 @@
 #!/usr/bin/env bash
 set -e
 
-server='ubuntu@34.254.239.115'
+#server='10.0.144.79'
+#proxy='52.210.164.117'
+#function sync {
+#  rsync 'ssh -o "ProxyCommand ssh -A $proxy -W %h:%p"' -avz ~/work/ambrosus/ambrosus-#ops/ansible "$server:~/" \
+#    --exclude=ambrosus-ops/.git/ \
+#    --exclude=ambrosus-ops/terraform
+#}
+
+server='ubuntu@52.210.95.251'
 
 function sync {
   rsync -avz ~/work/ambrosus/ambrosus-ops/ansible "$server:~/" \
