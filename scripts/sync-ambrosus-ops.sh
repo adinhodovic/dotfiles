cb463dc4a6a3854d225114a2fba8cb6d54f1e695
Adin Hodovic <hodovicadin@gmail.com>
2018-12-07 00:59:16 +0100

    upd

--- scripts/sync-ambrosus-ops.sh
+++ scripts/sync-ambrosus-ops.sh
@@ -6,7 +6,9 @@ server='52.16.114.112'
 function sync {
   rsync -avz ~/work/ambrosus/ambrosus-ops "$server:~/" \
     --exclude=ambrosus-ops/.git/ \
-    --exclude=ambrosus-ops/terraform
+    --exclude=ambrosus-ops/terraform/environments/dev/.terraform/ \
+    --exclude=ambrosus-ops/terraform/environments/ops/.terraform/ \
+    --exclude=ambrosus-ops/terraform/environments/test/.terraform/
   rsync -avz ~/.ssh/ambrosus_deployer_rsa "$server:~/.ssh/"
   rsync -avz ~/.vault-password "$server:~/"
 }
@@ -20,5 +22,5 @@ sync
 while inotifywait -e modify -r ~/work/ambrosus/ambrosus-ops/*; do
  notify-send 'Syncing to amb-ops...' -i network-transmit-receive
   sync
-  pkill xfce4-notifyd
+  #pkill xfce4-notifyd
 done
