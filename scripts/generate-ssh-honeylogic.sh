7b9e91a53adafc9d0adadfe281a2faae3c7737be
Adin Hodovic <hodovicadin@gmail.com>
2021-04-22 22:22:27 +0200

    Remove excess scripts

--- scripts/generate-ssh-honeylogic.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-set -ex
-AWS_DEFAULT_REGION=eu-west-1 populate-ssh-configs aws --prefix hl-ops --filters 'Name=tag:Environment,Values=ops' --jumphost jumphost --user adin  > ~/.ssh/config-hl-euw-ops
