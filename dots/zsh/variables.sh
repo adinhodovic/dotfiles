c8409b0fa63782ddede62b5dc38d060a794944d2
Adin Hodovic <hodovicadin@gmail.com>
2021-08-22 12:14:43 +0200

    Remove excess dotfiles variables

--- dots/zsh/variables.sh
+++ dots/zsh/variables.sh
@@ -9,8 +9,6 @@ export PATH="$PATH:/home/adin/.local/bin/"
 export PATH="$PATH:/home/adin/.gem/ruby/2.7.0/bin"
 export PATH="$PATH:/home/adin/.linkerd2/bin"
 export PATH="$PATH:/.local/share/nvim/plugged/vim-terraform-completion/bin/tffilter"
-export PATH="$PATH:/home/adin/work/tidepool/development/bin"
-export PATH="$PATH:/home/adin/work/tidepool/tpctl/cmd"
 
 export PYTHONPATH="/usr/local/lib/python3.9/site-packages:$PYTHONPATH"
 
@@ -18,11 +16,3 @@ export TERMINAL=/usr/bin/alacritty
 export TERM=screen-256color
 
 export RIPGREP_CONFIG_PATH=~/.ripgreprc
-
-NPM_PACKAGES="${HOME}/.npm-packages"
-export PATH="$NPM_PACKAGES/bin:$PATH"
-export NODE_PATH="${NPM_PACKAGES}/lib/node_modules"
-
-# Unset manpath so we can inherit from /etc/manpath via the `manpath` command
-unset MANPATH # delete if you already modified MANPATH elsewhere in your config
-export MANPATH="$NPM_PACKAGES/share/man:$(manpath)"
