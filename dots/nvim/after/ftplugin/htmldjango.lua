d96b6394a3971d4c85544d7f037b04bd54e04326
adinhodovic <hodovicadin@gmail.com>
2023-11-02 11:27:49 +0100

    fix: Djlint integration

--- dots/nvim/after/ftplugin/htmldjango.lua
+++ dots/nvim/after/ftplugin/htmldjango.lua
@@ -1,7 +1,8 @@
 local vim = vim
 
 vim.api.nvim_create_augroup("htmlDjango", {})
-vim.api.nvim_create_autocmd("BufWritePost", {
+vim.api.nvim_create_autocmd("BufWrite", {
+    pattern = "<buffer>",
     group = "htmlDjango",
     command = "call CocAction('format')",
     desc = "Format on save manually due to slow DjLint performance and timeouts"
