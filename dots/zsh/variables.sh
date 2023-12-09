8500d337ca439aacf0d5c6f29591408deb4492b2
adinhodovic <hodovicadin@gmail.com>
2023-12-09 22:47:44 +0100

    chore: Update dotfiles

--- dots/zsh/variables.sh
+++ dots/zsh/variables.sh
@@ -1,15 +1,19 @@
 ###############################
 # Set variables
 ###############################
-export PATH="$PATH:/usr/local/bin:/usr/local/sbin/:/home/adin/go/bin/"
+export PATH="$PATH:/usr/local/bin"
+export PATH="$PATH:/usr/local/sbin"
+export PATH="$PATH:$HOME/go/bin"
 export PATH="$PATH:$HOME/.poetry/bin"
 export PATH="$PATH:$HOME/.local/bin"
 export PATH="$PATH:$HOME/.krew/bin"
-export PATH="$PATH:/home/adin/.local/bin/"
-export PATH="$PATH:/home/adin/.gem/ruby/2.7.0/bin"
+export PATH="$PATH:$HOME/.local/bin"
+export PATH="$PATH:$HOME/.gem/ruby/2.7.0/bin"
 export PATH="$PATH:/.local/share/nvim/plugged/vim-terraform-completion/bin/tffilter"
 
 export PYTHONPATH="/usr/local/lib/python3.10/site-packages:$PYTHONPATH"
+
+export GOPATH="$HOME/go"
 
 export TERMINAL=/usr/bin/alacritty
 
