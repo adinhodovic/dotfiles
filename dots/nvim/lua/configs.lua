653f82ea86ca5382ae26499b3e132d7487ed533e
adinhodovic <hodovicadin@gmail.com>
2024-01-08 14:55:38 +0100

    chore: Update dotfiles

--- dots/nvim/lua/configs.lua
+++ dots/nvim/lua/configs.lua
@@ -227,14 +227,20 @@ set.timeoutlen = 500
 -------------------------------------------
 -- Go
 -------------------------------------------
-g.go_def_mapping_enabled = 0
+-- disable all linters as that is taken care of by coc.nvim
+g.go_diagnostics_enabled = 0
+g.go_metalinter_enabled = 0
+
+-- don't jump to errors after metalinter is invoked
+g.go_jump_to_error = 0
+
+-- automatically highlight variable your cursor is on
 g.go_auto_sameids = 0
+g.go_def_mapping_enabled = 0
 g.go_gopls_enabled = 0
 g.go_code_completion_enabled = 0
 g.go_fmt_autosave = 0
-g.go_diagnostics_enabled = 0
 g.go_echo_go_info = 0
-g.go_metalinter_enabled = 0
 g.go_metalinter_autosave = 0
 -- g.go_debug_windows = {
 -- vars = 'rightbelow 60vnew',
