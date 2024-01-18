472ddef0992766c163e37476d5bad5b8fbf3af2d
adinhodovic <hodovicadin@gmail.com>
2024-01-19 00:40:19 +0100

    chore: Update dotfiles

--- dots/nvim/lua/plugins.lua
+++ dots/nvim/lua/plugins.lua
@@ -93,6 +93,7 @@ Plug('gelguy/wilder.nvim', {
 Plug('Bekaboo/dropbar.nvim')                     -- Dropbar
 Plug('nvim-telescope/telescope-fzf-native.nvim') -- Telescope FZF
 Plug('akinsho/bufferline.nvim')                  -- Bufferline
+Plug('luukvbaal/statuscol.nvim')                 -- Statuscol
 -----------------------------------------
 --              Shortkeys
 -----------------------------------------
@@ -352,5 +353,58 @@ require('bufferline').setup({
 -- Spectre
 require('spectre').setup()
 
+-- Statuscol
+local builtin = require("statuscol.builtin")
+local cfg = {
+  segments = {
+    { text = { builtin.foldfunc, " " }, click = "v:lua.ScFa" },
+    { text = { "%s" },                  click = "v:lua.ScSa" },
+    {
+      text = { builtin.lnumfunc, " " },
+      condition = { true, builtin.not_empty },
+      click = "v:lua.ScLa",
+    }
+  },
+}
+require("statuscol").setup(cfg)
+
 -- UFO
-require('ufo').setup()
+local handler = function(virtText, lnum, endLnum, width, truncate)
+  local newVirtText = {}
+  local totalLines = vim.api.nvim_buf_line_count(0)
+  local foldedLines = endLnum - lnum
+  local suffix = ("  %d %d%%"):format(foldedLines, foldedLines / totalLines * 100)
+  local sufWidth = vim.fn.strdisplaywidth(suffix)
+  local targetWidth = width - sufWidth
+  local curWidth = 0
+  for _, chunk in ipairs(virtText) do
+    local chunkText = chunk[1]
+    local chunkWidth = vim.fn.strdisplaywidth(chunkText)
+    if targetWidth > curWidth + chunkWidth then
+      table.insert(newVirtText, chunk)
+    else
+      chunkText = truncate(chunkText, targetWidth - curWidth)
+      local hlGroup = chunk[2]
+      table.insert(newVirtText, { chunkText, hlGroup })
+      chunkWidth = vim.fn.strdisplaywidth(chunkText)
+      -- str width returned from truncate() may less than 2nd argument, need padding
+      if curWidth + chunkWidth < targetWidth then
+        suffix = suffix .. (" "):rep(targetWidth - curWidth - chunkWidth)
+      end
+      break
+    end
+    curWidth = curWidth + chunkWidth
+  end
+  local rAlignAppndx =
+      math.max(math.min(vim.opt.textwidth["_value"], width - 1) - curWidth - sufWidth, 0)
+  suffix = (" "):rep(rAlignAppndx) .. suffix
+  table.insert(newVirtText, { suffix, "MoreMsg" })
+  return newVirtText
+end
+
+-- global handler
+-- `handler` is the 2nd parameter of `setFoldVirtTextHandler`,
+-- check out `./lua/ufo.lua` and search `setFoldVirtTextHandler` for detail.
+require('ufo').setup({
+  fold_virt_text_handler = handler
+})
