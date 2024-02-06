a280c2ba3a20f309ea4f917336aa7df3d6d340c2
adinhodovic <hodovicadin@gmail.com>
2024-02-06 23:07:09 +0100

    chore: Remove after

--- dots/nvim/after/ftplugin/htmldjango.lua
+++
@@ -1,9 +0,0 @@
--- local vim = vim
-
--- vim.api.nvim_create_augroup("htmlDjango", {})
--- vim.api.nvim_create_autocmd("BufWrite", {
--- pattern = "<buffer>",
--- group = "htmlDjango",
--- command = "call CocAction('format')",
--- desc = "Format on save manually due to slow DjLint performance and timeouts"
--- })
