b27bfc69b22fd2d99a5bc857238938e4b393ea96
adinhodovic <hodovicadin@gmail.com>
2024-02-10 18:48:49 +0100

    chore: Update dotfiles

--- dots/nvim/lua/plugins/git.lua
+++ dots/nvim/lua/plugins/git.lua
@@ -146,4 +146,22 @@ return {
 		},
 		opts = {},
 	},
+	{
+		"aaronhallaert/ts-advanced-git-search.nvim",
+		config = function()
+			require("telescope").load_extension("advanced_git_search")
+		end,
+		dependencies = {
+			"nvim-telescope/telescope.nvim",
+			-- to show diff splits and open commits in browser
+			"tpope/vim-fugitive",
+		},
+		keys = {
+			{
+				"<leader>gsa",
+				"<cmd>Telescope advanced_git_search show_custom_functions<cr>",
+				desc = "Advanced Git Search",
+			},
+		},
+	},
 }
