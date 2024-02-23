ec7ccbf693df93821d17b0037dda8f95634be009
adinhodovic <hodovicadin@gmail.com>
2024-02-23 11:35:18 +0100

    chore: Update dotfiles

--- dots/nvim/lua/plugins/formatting.lua
+++ dots/nvim/lua/plugins/formatting.lua
@@ -11,10 +11,13 @@ return {
 		enabled = true,
 		config = function()
 			require("conform").setup({
-				format_on_save = {
-					timeout_ms = 500,
-					lsp_fallback = true,
-				},
+				format_on_save = function(bufnr)
+					-- Disable with a global or buffer-local variable
+					if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+						return
+					end
+					return { timeout_ms = 500, lsp_fallback = true }
+				end,
 				formatters = {
 					tk = {
 						command = vim.fn.systemlist("which tk")[1],
@@ -45,6 +48,24 @@ return {
 					yaml = { "yamlfmt" },
 					html = { "djlint" },
 				},
+			})
+
+			vim.api.nvim_create_user_command("FormatDisable", function(args)
+				if args.bang then
+					-- FormatDisable! will disable formatting just for this buffer
+					vim.b.disable_autoformat = true
+				else
+					vim.g.disable_autoformat = true
+				end
+			end, {
+				desc = "Disable autoformat-on-save",
+				bang = true,
+			})
+			vim.api.nvim_create_user_command("FormatEnable", function()
+				vim.b.disable_autoformat = false
+				vim.g.disable_autoformat = false
+			end, {
+				desc = "Re-enable autoformat-on-save",
 			})
 		end,
 	},
