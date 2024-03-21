f7aa7ea3dacc585566849c7023c96b4acf2b8dfb
adinhodovic <hodovicadin@gmail.com>
2024-03-21 13:06:05 +0100

    chore: Update dotfiles

--- dots/nvim/lua/plugins/automation.lua
+++ dots/nvim/lua/plugins/automation.lua
@@ -485,4 +485,32 @@ return {
 			},
 		},
 	},
+	{
+		"chrisgrieser/nvim-spider",
+		keys = {
+			{
+				"w",
+				"<cmd>lua require('spider').motion('w')<CR>",
+				mode = { "n", "o", "x" },
+			},
+			{
+				"e",
+				"<cmd>lua require('spider').motion('e')<CR>",
+				mode = { "n", "o", "x" },
+			},
+			-- ...
+			{
+				"q",
+				"<cmd>lua require('spider').motion('b')<CR>",
+				mode = { "n", "o", "x" },
+			},
+		},
+		dependencies = {
+			"theHamsta/nvim_rocks",
+			build = "pip3 install --user hererocks && python3 -mhererocks . -j2.1.0-beta3 -r3.0.0 && cp nvim_rocks.lua lua",
+			config = function()
+				require("nvim_rocks").ensure_installed("luautf8")
+			end,
+		},
+	},
 }
