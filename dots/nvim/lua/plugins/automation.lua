694104b99ecc0a493a0ec81205aff11d60b29dca
adinhodovic <hodovicadin@gmail.com>
2024-05-09 13:01:30 +0000

    chore: Update dotfiles

--- dots/nvim/lua/plugins/automation.lua
+++ dots/nvim/lua/plugins/automation.lua
@@ -172,7 +172,7 @@ return {
 		},
 		dependencies = {
 			"theHamsta/nvim_rocks",
-			build = "pipx install hererocks && python3 -mhererocks . -j2.1.0-beta3 -r3.0.0 && cp nvim_rocks.lua lua",
+			build = "pipx install hererocks && hererocks . -j2.1.0-beta3 -r3.0.0 && cp nvim_rocks.lua lua",
 			config = function()
 				require("nvim_rocks").ensure_installed("luautf8")
 			end,
