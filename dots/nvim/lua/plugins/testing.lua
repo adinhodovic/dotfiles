17f99910782d2463e236d6d7146af5401a59ef3f
adinhodovic <hodovicadin@gmail.com>
2024-07-19 13:43:34 +0200

    feat: Add neotest-go

--- dots/nvim/lua/plugins/testing.lua
+++ dots/nvim/lua/plugins/testing.lua
@@ -8,11 +8,25 @@ return {
 			"antoinemadec/FixCursorHold.nvim",
 			"nvim-treesitter/nvim-treesitter",
 			"nvim-neotest/neotest-python",
+			"nvim-neotest/neotest-go",
 		},
 		config = function()
+			-- get neotest namespace (api call creates or returns namespace)
+			local neotest_ns = vim.api.nvim_create_namespace("neotest")
+			vim.diagnostic.config({
+				virtual_text = {
+					format = function(diagnostic)
+						local message =
+							diagnostic.message:gsub("\n", " "):gsub("\t", " "):gsub("%s+", " "):gsub("^%s+", "")
+						return message
+					end,
+				},
+			}, neotest_ns)
+
 			require("neotest").setup({
 				adapters = {
 					require("neotest-python"),
+					require("neotest-go"),
 				},
 			})
 		end,
