33839300fccfd2980d8a4953781e5f81c15b32c7
adinhodovic <hodovicadin@gmail.com>
2024-08-25 00:25:35 +0200

    chore: Update dotfiles

--- dots/zsh/alias.sh
+++ dots/zsh/alias.sh
@@ -156,7 +156,6 @@ help() {
 }
 
 # Use bat for help
-alias -g -- -h='-h 2>&1 | bat --language=help --style=plain'
 alias -g -- --help='--help 2>&1 | bat --language=help --style=plain'
 
 function vf {
