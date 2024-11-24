8205b90b16272ca941227e26752bc81deb65b174
adinhodovic <hodovicadin@gmail.com>
2024-11-24 12:59:48 +0100

    chore: Update dotfiles

--- dots/nvim/lua/plugins/automation.lua
+++ dots/nvim/lua/plugins/automation.lua
@@ -329,4 +329,26 @@ return {
 			})
 		end,
 	},
+	{
+		"ThePrimeagen/refactoring.nvim",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-treesitter/nvim-treesitter",
+			"nvim-telescope/telescope.nvim",
+		},
+		config = function()
+			require("refactoring").setup()
+			require("telescope").load_extension("refactoring")
+		end,
+		keys = {
+			{
+				"<leader>rr",
+				function()
+					require("telescope").extensions.refactoring.refactors()
+				end,
+				mode = { "n", "x" },
+				desc = "Refactor: Telescope",
+			},
+		},
+	},
 }
