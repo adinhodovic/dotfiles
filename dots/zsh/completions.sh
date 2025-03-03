4e7d27c332470e3d895e4bb2c978190355385ffe
adinhodovic <hodovicadin@gmail.com>
2025-03-03 12:03:25 +0100

    chore: Update dotfiles

--- dots/zsh/completions.sh
+++ dots/zsh/completions.sh
@@ -3,5 +3,9 @@ eval "$(register-python-argcomplete pipx)"
 source <(authzed completion zsh)
 source <(zed completion zsh)
 
+source /home/adin/.config/broot/launcher/bash/br
+
+complete -o nospace -C "$(which tk)" tk
+
 # We use kubecolor
 compdef kubecolor=kubectl
--- dots/zsh/variables.sh
+++ dots/zsh/variables.sh
@@ -12,6 +12,7 @@ export PATH="$PATH:$HOME/.local/bin"
 export PATH="$PATH:$HOME/.gem/ruby/2.7.0/bin"
 export PATH="$PATH:$HOME/.local/share/nvim/mason/bin"
 export PATH="$PATH:$HOME/.pulumi/bin"
+export PATH="$PATH:$HOME/.grit/bin"
 
 export EDITOR="nvim"
 
