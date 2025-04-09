12c92683e0b774e84b394b59a6a6aa1bafbe6d5a
adinhodovic <hodovicadin@gmail.com>
2025-04-09 10:27:05 +0200

    chore: Update dotfiles

--- dots/nvim/lua/plugins/ai.lua
+++ dots/nvim/lua/plugins/ai.lua
@@ -54,18 +54,21 @@ return {
 			behaviour = {
 				auto_suggestions = false,
 			},
+			file_selector = {
+				provider = "fzf",
+			},
 		},
 		build = "make",
 		dependencies = {
+			"nvim-treesitter/nvim-treesitter",
 			"stevearc/dressing.nvim",
 			"nvim-lua/plenary.nvim",
 			"MunifTanjim/nui.nvim",
 			--- The below dependencies are optional,
 			"hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
+			"ibhagwan/fzf-lua", -- for file_selector provider fzf
 			"nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
 			"zbirenbaum/copilot.lua", -- for providers='copilot'
-			"MeanderingProgrammer/render-markdown.nvim",
-			"HakonHarnes/img-clip.nvim",
 		},
 	},
 }
